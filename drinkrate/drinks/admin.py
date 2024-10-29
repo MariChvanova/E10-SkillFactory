0186de118bb0c1dc043f39fe31f643993876b1d7
Мария <chvanovamaria@gmail.com>
2024-10-29 11:18:55 +0500

    1

--- drinkrate/drinks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Drink
-
-
-@admin.register(Drink)
-class DrinkAdmin(admin.ModelAdmin):
-    fields = ['name', 'description', 'rating']
--- drinkrate/drinks/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class Drink(models.Model):
-    name = models.CharField('название', max_length=128)
-    description = models.TextField('описание', null=True)
-    rating = models.IntegerField()
